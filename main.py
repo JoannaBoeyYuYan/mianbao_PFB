7376006a14c0ec619352a996870fb19a8ab39f11
jiaying23 <s10261025@connect.np.edu.sg>
2023-08-10 16:35:23 +0800

    summary text write finish already

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from pathlib import Path
 import csv 
 import overheads 
 import cash_on_hand 
-# import profit_and_loss 
+import profit_and_loss 
 
 def main(): 
     highest_category, max_overhead  = overheads.overheads_function() 
     cash_on_hand_output = cash_on_hand.cash_on_hand_function() 
-    # profit_and_loss.profit_and_Loss_function()
+    profit_deficit_output = profit_and_loss.profit_and_loss_function()
 
     # Create a path object for summary_report.txt
     fp = Path.cwd() / "summary_report.txt" 
@@ -20,5 +20,14 @@ def main():
             flag, day, amount = result
             cash_line = f"[CASH {flag}] DAY: {day}, AMOUNT: USD {amount}\n"
             file.write(cash_line)
+
+        for deficit_line in profit_deficit_output:
+            file.write(deficit_line + '\n') 
+
+    # # print the results to read here
+    # with fp.open(mode='r', encoding ='UTF-8', newline = "") as file:
+    #     reader = csv.reader(file)
+    #     for line in reader:
+    #         print(line)
 
 main()
--- profit_and_loss.py
+++ profit_and_loss.py
@@ -1,77 +1,83 @@
-from pathlib import Path
-import csv
+def profit_and_loss_function():
+    from pathlib import Path
+    import csv
 
-# Set the file path to "Profit_And_Loss.csv"
-file_path = Path.cwd()/'csv_reports'/"Profit_And_Loss.csv"
+    # Set the file path to "Profit_And_Loss.csv"
+    file_path = Path.cwd()/'csv_reports'/"Profit_And_Loss.csv"
 
-# Convert the file path to a Path object
-fp = Path(file_path)
+    # Convert the file path to a Path object
+    fp = Path(file_path)
 
-# Read the csv file to append profit and quantity from the csv.
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    # Skip header
-    next(reader) 
+    # Read the csv file to append profit and quantity from the csv.
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        # Skip header
+        next(reader) 
 
-    # Create an empty list to store profit and loss
-    Profit_And_Loss = [] 
+        # Create an empty list to store profit and loss
+        Profit_And_Loss = [] 
 
-    # Append Profit_And_Loss record into the Profit_And_Loss list
-    for row in reader:
-        # Get the Day, Sales, Trading Profit, Operating Expense, and Net Profit for each record
-        # and append the Profit_And_Loss list
-        Profit_And_Loss.append([row[0], row[1], row[2], row[3], row[4]])   
+        # Append Profit_And_Loss record into the Profit_And_Loss list
+        for row in reader:
+            # Get the Day, Sales, Trading Profit, Operating Expense, and Net Profit for each record
+            # and append the Profit_And_Loss list
+            Profit_And_Loss.append([row[0], row[1], row[2], row[3], row[4]])   
+        
+        aList = []
+
+    # Compute the difference in net profit column
+    previous_net_profit = 0
+    highest_surplus_day = None
+    highest_surplus_amount = 0
+    profit_deficit_day = None
+    profit_deficit_amount = 0
+    # Initialize a counter for the days with higher net profit than the previous day
+    higher_net_profit_days = 0
+    for record in Profit_And_Loss:
+        day, sales, trading_profit, operating_expense, net_profit = record
+
+        # Convert net profit to a numeric value (remove commas and convert to float)
+        net_profit = float(net_profit.replace(",", ""))
+
+        # Compute the difference between current net profit and previous net profit
+        profit_difference = net_profit - previous_net_profit
+        previous_net_profit = net_profit
+
+        # Check if the net profit is higher, lower, or the same as the previous day
+        if profit_difference > 0:
+            comparison = "HIGHER"
+            # Increment the counter for higher net profit days
+            higher_net_profit_days += 1  
+        #Check if net profit is negative(indicating a deficit)
+        elif profit_difference < 0:
+            comparison = "LOWER"
+            # Append the deficit day and amount to the list
+            aList.append([day, profit_difference])
+        else:
+            comparison = "-"
+
+        # Check if the current surplus is the highest so far
+        if profit_difference > highest_surplus_amount:
+            highest_surplus_day = day
+            highest_surplus_amount = profit_difference
+        # Check if the current deficit is the lowest so far
+        if profit_difference < profit_deficit_amount:
+            profit_deficit_day = day
+            profit_deficit_amount = profit_difference
+
+#     # Print the profit deficit information from the list
+#     for result in aList:
+#         deficit_day, deficit_amount = result
+#         print(f"[PROFIT DEFICIT] DAY: {deficit_day}, AMOUNT: USD{abs(int(deficit_amount))}")
+
+    deficit_output = []  # Create an empty list to store deficit information
+
+    # Print the profit deficit information from the list
+    for result in aList:
+        deficit_day, deficit_amount = result
+        deficit_line = f"[PROFIT DEFICIT] DAY: {deficit_day}, AMOUNT: USD{abs(int(deficit_amount))}"
+        deficit_output.append(deficit_line)  # Append deficit line to the output list
     
-    aList = []
-
-# Compute the difference in net profit column
-previous_net_profit = 0
-highest_surplus_day = None
-highest_surplus_amount = 0
-profit_deficit_day = None
-profit_deficit_amount = 0
-# Initialize a counter for the days with higher net profit than the previous day
-higher_net_profit_days = 0
-for record in Profit_And_Loss:
-    day, sales, trading_profit, operating_expense, net_profit = record
-
-    # Convert net profit to a numeric value (remove commas and convert to float)
-    net_profit = float(net_profit.replace(",", ""))
-
-    # Compute the difference between current net profit and previous net profit
-    profit_difference = net_profit - previous_net_profit
-    previous_net_profit = net_profit
-
-    # Check if the net profit is higher, lower, or the same as the previous day
-    if profit_difference > 0:
-        comparison = "HIGHER"
-        # Increment the counter for higher net profit days
-        higher_net_profit_days += 1  
-    #Check if net profit is negative(indicating a deficit)
-    elif profit_difference < 0:
-        comparison = "LOWER"
-        # Append the deficit day and amount to the list
-        aList.append([day, profit_difference])
-    else:
-        comparison = "-"
-
-    # Check if the current surplus is the highest so far
-    if profit_difference > highest_surplus_amount:
-        highest_surplus_day = day
-        highest_surplus_amount = profit_difference
-    # Check if the current deficit is the lowest so far
-    if profit_difference < profit_deficit_amount:
-        profit_deficit_day = day
-        profit_deficit_amount = profit_difference
-
-# Print the profit deficit information from the list
-for result in aList:
-    deficit_day, deficit_amount = result
-    print(f"[PROFIT DEFICIT] DAY: {deficit_day}, AMOUNT: USD{abs(int(deficit_amount))}")
+    return deficit_output  # Return the list of deficit information
 
 
-# print out the net profit surplus, highest net profit surplus [day and amount], and profit deficit [day and amount]
-print("\n")
-print(f"[NET PROFIT SURPLUS]: NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
-print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {highest_surplus_day}, AMOUNT: USD{int(highest_surplus_amount)}")
-print(f"[PROFIT DEFICIT] DAY: {profit_deficit_day}, AMOUNT: USD{abs(int(profit_deficit_amount))}")
