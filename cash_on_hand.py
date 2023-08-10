017a61dd5ee6f0f52e833b114662383d03c0bed2
jiaying23 <s10261025@connect.np.edu.sg>
2023-08-10 14:54:40 +0800

    update

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,7 +2,6 @@ def cash_on_hand_function():
     from pathlib import Path
     import csv
 
-<<<<<<< HEAD
     # create a file to csv file.
     fp = Path.cwd()/'csv_reports'/"cash_on_hand.csv"
     #print(fp.exists()) - To check if file exsists
@@ -76,62 +75,3 @@ def cash_on_hand_function():
     # print(f"[CASH SURPLUS]: NET PROFIT ON EACH DAY ARE HIGHER THAN PREVIOUS DAY")
     # print(f"[HIGHEST CASH SURPLUS] DAY: {highest_surplus_day}, AMOUNT: USD{int(highest_surplus_amount)}")
     # print(f"[CASH DEFICIT] DAY: {cash_deficit_day}, AMOUNT: USD{abs(int(cash_deficit_amount))}")
-=======
-# create a file to csv file.
-fp = Path.cwd() / 'csv_reports' / "cash_on_hand.csv"
-
-coh = []
-
-# read the csv file to append days and cash on hand from the csv
-with fp.open(mode='r', encoding='UTF-8', newline="") as file:
-    reader = csv.reader(file)
-    next(reader)  # skip header
-    for row in reader:
-        coh.append([row[0], row[1]])
-
-# create a function to calculate difference to determine whether it is deficits or surplus
-def coh_diff(coh):
-    flag_list = []
-    prev = int(coh[0][1])
-
-    for days in range(1, len(coh)):
-        cash = int(coh[days][1])
-        diff = cash - prev
-        prev = cash
-
-        if diff < 0:
-            flag = 'DEFICITS'
-        elif diff > 0:
-            flag = 'SURPLUS'
-        else:
-            flag = 'NO SURPLUS/DEFICITS'
-
-        if diff < 0:
-            diff = diff * (-1)
-
-        if flag == 'DEFICITS':
-            flag_list.append([flag, coh[days][0], diff])
-
-    return flag_list
-
-coh_results = coh_diff(coh)
-
-highest_cash_surplus = 0
-highest_cash_day = ""
-cash_deficit_amount = 0
-cash_deficit_day = ""
-
-for result in coh_results:
-    if result[0] == "SURPLUS":
-        if result[2] > highest_cash_surplus:
-            highest_cash_surplus = result[2]
-            highest_cash_day = result[1]
-    elif result[0] == "DEFICITS":
-        if result[2] > cash_deficit_amount:
-            cash_deficit_amount = result[2]
-            cash_deficit_day = result[1]
-
-for result in coh_results:
-    print(f"[CASH {result[0]}] DAY: {result[1]} , AMOUNT: USD {result[2]}")
-
->>>>>>> bdbaf467cd5cbe88f1fdb9aa8bcccc4fef9beb31
