38a0f21ac25ee60ead1806c9a38869f45dbdc5c4
jiaying23 <s10261025@connect.np.edu.sg>
2023-08-10 16:10:30 +0800

    text

--- main2.py
+++ main2.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import csv 
 import overheads 
-# import cash_on_hand 
+import cash_on_hand 
 # import profit_and_loss 
 
 def main(): 
     highest_category, max_overhead  = overheads.overheads_function() 
-    # cash_on_hand_output = cash_on_hand.cash_on_hand_function() 
+    cash_on_hand_output = cash_on_hand.cash_on_hand_function() 
     # profit_and_loss.profit_and_Loss_function()
 
     # Create a path object for summary_report.txt
@@ -16,13 +16,9 @@ def main():
     with fp.open(mode='w', encoding='UTF-8', newline="") as file:
         file.write(f"[HIGHEST OVERHEAD] {highest_category}: {max_overhead} % \n" )
 
-        # for result in cash_on_hand_output:
-        #     flag, day, amount = result
-        #     cash_line = f"[CASH {flag}] DAY: {day}, AMOUNT: USD {amount}\n"
-        #     file.write(cash_line)
-
-            
-# if __name__ == "__main__":
-#     main()
+        for result in cash_on_hand_output:
+            flag, day, amount = result
+            cash_line = f"[CASH {flag}] DAY: {day}, AMOUNT: USD {amount}\n"
+            file.write(cash_line)
 
 main()
